be80f388a20a30f2e6fb9e1ac943ffd4e5a33ae3
Stephen Celis <stephen@stephencelis.com>
2017-06-18 12:48:02 -0400

    Move tests, indent Package.swift

--- Package.swift
+++ Package.swift
@@ -2,14 +2,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "Prelude",
-    products: [
-        .library(name: "Prelude", targets: ["Prelude"]),
-    ],
-    dependencies: [
-    ],
-    targets: [
-        .target(name: "Prelude", dependencies: []),
-        .testTarget(name: "PreludeTests", dependencies: ["Prelude"]),
-    ]
+  name: "Prelude",
+  products: [
+    .library(name: "Prelude", targets: ["Prelude"]),
+  ],
+  dependencies: [
+  ],
+  targets: [
+    .target(name: "Prelude", dependencies: []),
+    .testTarget(name: "PreludeTests", dependencies: ["Prelude"]),
+  ]
 )
--- Tests/PreludeTests/ArrayTests.swift
+++ Tests/PreludeTests/ArrayTests.swift
@@ -2,11 +2,4 @@ import XCTest
 import Prelude
 
 class ArrayTests: XCTestCase {
-  func testCatOptionals() {
-    XCTAssertEqual([1, 2, 3], [1, nil, 2, nil, 3] |> catOptionals)
-  }
-
-  func testMapOptional() {
-    XCTAssertEqual([2], [1, 2, 3] |> mapOptional { $0 % 2 == 0 ? $0 : nil })
-  }
 }
