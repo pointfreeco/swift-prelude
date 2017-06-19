b226b346f93a2fcd42cd05e778fdc5f2430b908a
Stephen Celis <stephen.celis@gmail.com>
2017-06-19 09:27:10 -0400

    Add KeyPath lens-like functions (#6)

---
+++ Sources/Prelude/KeyPath.swift
@@ -0,0 +1,23 @@
+public func get<Root, Value>(_ keyPath: KeyPath<Root, Value>) -> (Root) -> Value {
+  return { root in
+    root[keyPath: keyPath]
+  }
+}
+
+public func over<Root, Value>(_ keyPath: WritableKeyPath<Root, Value>)
+  -> (@escaping (Value) -> Value)
+  -> (Root)
+  -> Root {
+
+    return { over in
+      { root in
+        var copy = root
+        copy[keyPath: keyPath] = over(copy[keyPath: keyPath])
+        return copy
+      }
+    }
+}
+
+public func set<Root, Value>(_ keyPath: WritableKeyPath<Root, Value>) -> (Value) -> (Root) -> Root {
+  return over(keyPath) <<< const
+}
