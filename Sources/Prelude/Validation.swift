e6b6cf8838fb1539e60f3bc5988b8b7da5a78123
Stephen Celis <stephen@stephencelis.com>
2017-06-25 12:57:06 -0400

    Validation type

---
+++ Sources/Prelude/Validation.swift
@@ -0,0 +1,133 @@
+public enum Validation<E, A> {
+  case valid(A)
+  case invalid(E)
+}
+
+// MARK: - Functor
+
+extension Validation {
+  public func map<B>(_ a2b: (A) -> B) -> Validation<E, B> {
+    switch self {
+    case let .valid(a):
+      return .valid(a2b(a))
+    case let .invalid(e):
+      return .invalid(e)
+    }
+  }
+
+  public static func <¢> <B>(a2b: (A) -> B, a: Validation) -> Validation<E, B> {
+    return a.map(a2b)
+  }
+}
+
+public func map<A, B, C>(_ b2c: @escaping (B) -> C)
+  -> (Validation<A, B>)
+  -> Validation<A, C> {
+    return { ab in
+      b2c <¢> ab
+    }
+}
+
+// MARK: - Apply
+
+extension Validation where E: Semigroup {
+  public func apply<B>(_ a2b: Validation<E, (A) -> B>) -> Validation<E, B> {
+    switch (a2b, self) {
+    case let (.valid(f), _):
+      return self.map(f)
+    case let (.invalid(e), .valid):
+      return .invalid(e)
+    case let (.invalid(e1), .invalid(e2)):
+      return .invalid(e1 <> e2)
+    }
+  }
+
+  public static func <*> <B>(a2b: Validation<E, (A) -> B>, a: Validation) -> Validation<E, B> {
+    return a.apply(a2b)
+  }
+}
+
+public func apply<A: Semigroup, B, C>(_ b2c: Validation<A, (B) -> C>)
+  -> (Validation<A, B>)
+  -> Validation<A, C> {
+    return { ab in
+      b2c <*> ab
+    }
+}
+
+// MARK: - Applicative
+
+public func pure<E, A>(_ a: A) -> Validation<E, A> {
+  return .valid(a)
+}
+
+// MARK: - Alt
+
+public extension Validation where E: Semigroup {
+  public static func <|> (lhs: Validation, rhs: Validation) -> Validation {
+    switch (lhs, rhs) {
+    case (.invalid, .valid):
+      return rhs
+    case let (.invalid(e1), .invalid(e2)):
+      return .invalid(e1 <> e2)
+    default:
+      return lhs
+    }
+  }
+}
+
+// MARK: - Eq/Equatable
+
+extension Validation where E: Equatable, A: Equatable {
+  public static func == (lhs: Validation, rhs: Validation) -> Bool {
+    switch (lhs, rhs) {
+    case let (.invalid(e1), .invalid(e2)):
+      return e1 == e2
+    case let (.valid(a1), .valid(a2)):
+      return a1 == a2
+    default:
+      return false
+    }
+  }
+
+  public static func != (lhs: Validation, rhs: Validation) -> Bool {
+    return !(lhs == rhs)
+  }
+}
+
+// MARK: - Ord/Comparable
+
+extension Validation where E: Comparable, A: Comparable {
+  public static func < (lhs: Validation, rhs: Validation) -> Bool {
+    switch (lhs, rhs) {
+    case let (.invalid(e1), .invalid(e2)):
+      return e1 < e2
+    case let (.valid(a1), .valid(a2)):
+      return a1 < a2
+    case (.invalid, .valid):
+      return true
+    case (.valid, .invalid):
+      return false
+    }
+  }
+
+  public static func <= (lhs: Validation, rhs: Validation) -> Bool {
+    return lhs < rhs || lhs == rhs
+  }
+
+  public static func > (lhs: Validation, rhs: Validation) -> Bool {
+    return !(lhs <= rhs)
+  }
+
+  public static func >= (lhs: Validation, rhs: Validation) -> Bool {
+    return lhs > rhs || lhs == rhs
+  }
+}
+
+// MARK: - Semigroup
+
+extension Validation where E: Semigroup, A: Semigroup {
+  public static func <> (lhs: Validation, rhs: Validation) -> Validation {
+    return curry(<>) <¢> lhs <*> rhs
+  }
+}
