56f7a73475cda3e0b45f2254e719342e0906b037
Stephen Celis <stephen@stephencelis.com>
2017-06-25 11:33:04 -0400

    Some Either/Optional goodies

--- Sources/Prelude/Optional.swift
+++ Sources/Prelude/Optional.swift
@@ -1,3 +1,11 @@
-public func coalesce<A>(with `default`: A) -> (A?) -> A {
-  return { x in x ?? `default` }
+public func optional<A, B>(_ default: B) -> (@escaping (A) -> B) -> (A?) -> B {
+  return { a2b in
+    { a in
+      a.map(a2b) ?? `default`
+    }
+  }
+}
+
+public func coalesce<A>(with default: A) -> (A?) -> A {
+  return optional(`default`) <| id
 }
