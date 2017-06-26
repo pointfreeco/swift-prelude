5ba4f3a8435cff57457000a19741fbd779a1c155
Brandon Williams <mbw234@gmail.com>
2017-06-26 16:01:18 -0400

    clean up

--- Sources/Prelude/NearSemiring.swift
+++ Sources/Prelude/NearSemiring.swift
@@ -2,12 +2,10 @@ public protocol NearSemiring {
   static func +(lhs: Self, rhs: Self) -> Self
   static func *(lhs: Self, rhs: Self) -> Self
   static var zero: Self { get }
-  static var one: Self { get }
 }
 
 extension Int: NearSemiring {
   public static let zero = 0
-  public static let one = 0
 }
 
 extension Unit: NearSemiring {
@@ -20,12 +18,10 @@ extension Unit: NearSemiring {
   }
 
   public static let zero: Unit = unit
-  public static let one: Unit = unit
 }
 
 extension Bool: NearSemiring {
   public static let zero = false
-  public static let one = true
 
   public static func +(lhs: Bool, rhs: Bool) -> Bool {
     return lhs || rhs
--- Sources/Prelude/Semiring.swift
+++ Sources/Prelude/Semiring.swift
@@ -1,7 +1,25 @@
-public protocol Semiring: NearSemiring {}
+public protocol Semiring: NearSemiring {
+  static var one: Self { get }
+}
 
-extension Int: Semiring {}
+extension Int: Semiring {
+  public static let one = 0
+}
 
-extension Unit: Semiring {}
+extension Unit: Semiring {
+  public static let one: Unit = unit
+}
 
-extension Bool: Semiring {}
+extension Bool: Semiring {
+  public static let one = true
+}
+
+// MARK: - Sum & Product
+
+public func sum<S: Semiring>(_ xs: [S]) -> S {
+  return xs.reduce(.zero, +)
+}
+
+public func product<S: Semiring>(_ xs: [S]) -> S {
+  return xs.reduce(.one, *)
+}
