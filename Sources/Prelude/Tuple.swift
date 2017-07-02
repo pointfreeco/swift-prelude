cf32f9da24ff606428b0a76b5adebe2e5011d5c0
Stephen Celis <stephen@stephencelis.com>
2017-07-01 20:46:51 -0400

    Tuple conformances

--- Sources/Prelude/Tuple.swift
+++ Sources/Prelude/Tuple.swift
@@ -5,3 +5,25 @@ public func first<A, B>(_ x: (A, B)) -> A {
 public func second<A, B>(_ x: (A, B)) -> B {
   return x.1
 }
+
+// MARK: - Semigroupoid
+
+public func >>> <A, B, C>(_ ab: (A, B), _ bc: (B, C)) -> (A, C) {
+  return (ab.0, bc.1)
+}
+
+public func <<< <A, B, C>(_ bc: (B, C), _ ab: (A, B)) -> (A, C) {
+  return (ab.0, bc.1)
+}
+
+// MARK: - Semigroup
+
+public func <> <A: Semigroup, B: Semigroup>(_ ab1: (A, B), _ ab2: (A, B)) -> (A, B) {
+  return (ab1.0 <> ab2.0, ab1.1 <> ab2.1)
+}
+
+// MARK: - Monoid
+
+public func empty<A: Monoid, B: Monoid>() -> (A, B) {
+  return (A.empty, B.empty)
+}
