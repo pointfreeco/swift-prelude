51be92baf40584f1a61db2606f69051823da6052
Stephen Celis <stephen@stephencelis.com>
2017-07-03 17:43:15 -0400

    Order disassociative precedence groups

--- Sources/Prelude/PrecedenceGroups.swift
+++ Sources/Prelude/PrecedenceGroups.swift
@@ -1,82 +1,82 @@
-precedencegroup infixr0 {
-  associativity: right
-  higherThan: AssignmentPrecedence
-}
-precedencegroup infixr1 {
-  associativity: right
-  higherThan: infixr0
-}
-precedencegroup infixr2 {
-  associativity: right
-  higherThan: infixr1
-}
-precedencegroup infixr3 {
-  associativity: right
-  higherThan: infixr2
-}
-precedencegroup infixr4 {
-  associativity: right
-  higherThan: infixr3
-}
-precedencegroup infixr5 {
-  associativity: right
-  higherThan: infixr4
-  lowerThan: AdditionPrecedence
-}
-precedencegroup infixr6 {
-  associativity: right
-  higherThan: AdditionPrecedence
-  lowerThan: MultiplicationPrecedence
-}
-precedencegroup infixr7 {
-  associativity: right
-  higherThan: MultiplicationPrecedence
-}
-precedencegroup infixr8 {
-  associativity: right
-  higherThan: infixr7
-}
-precedencegroup infixr9 {
-  associativity: right
-  higherThan: infixr8
-}
-
 precedencegroup infixl0 {
   associativity: left
+  higherThan: AssignmentPrecedence
+}
+precedencegroup infixr0 {
+  associativity: right
+  higherThan: infixl0
 }
 precedencegroup infixl1 {
   associativity: left
-  higherThan: infixl0
+  higherThan: infixr0
+}
+precedencegroup infixr1 {
+  associativity: right
+  higherThan: infixl1
 }
 precedencegroup infixl2 {
   associativity: left
-  higherThan: infixl1
+  higherThan: infixr1
+}
+precedencegroup infixr2 {
+  associativity: right
+  higherThan: infixl2
 }
 precedencegroup infixl3 {
   associativity: left
-  higherThan: infixl2
+  higherThan: infixr2
+}
+precedencegroup infixr3 {
+  associativity: right
+  higherThan: infixl3
 }
 precedencegroup infixl4 {
   associativity: left
-  higherThan: infixl3
+  higherThan: infixr3
+}
+precedencegroup infixr4 {
+  associativity: right
+  higherThan: infixl4
 }
 precedencegroup infixl5 {
   associativity: left
-  higherThan: infixl4
+  higherThan: infixr4
+}
+precedencegroup infixr5 {
+  associativity: right
+  higherThan: infixl5
+  lowerThan: AdditionPrecedence
 }
 precedencegroup infixl6 {
   associativity: left
-  higherThan: infixl5
+  higherThan: AdditionPrecedence
+}
+precedencegroup infixr6 {
+  associativity: right
+  higherThan: infixl6
+  lowerThan: MultiplicationPrecedence
 }
 precedencegroup infixl7 {
   associativity: left
-  higherThan: infixl6
+  higherThan: MultiplicationPrecedence
+}
+precedencegroup infixr7 {
+  associativity: right
+  higherThan: infixl7
 }
 precedencegroup infixl8 {
   associativity: left
+  higherThan: infixr7
+}
+precedencegroup infixr8 {
+  associativity: right
   higherThan: infixl7
 }
 precedencegroup infixl9 {
   associativity: left
-  higherThan: infixl8
+  higherThan: infixr8
+}
+precedencegroup infixr9 {
+  associativity: right
+  higherThan: infixl9
 }
