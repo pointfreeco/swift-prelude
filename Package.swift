887fefca757027e298de69b7bcdece3d514fde9c
Brandon Williams <mbw234@gmail.com>
2017-07-06 07:44:47 -0400

    formatting

--- Package.swift
+++ Package.swift
@@ -7,15 +7,16 @@ let package = Package(
     .library(name: "Either", targets: ["Either"]),
     .library(name: "Optics", targets: ["Optics"]),
     .library(name: "Prelude", targets: ["Prelude"]),
-  ],
+    ],
   dependencies: [
   ],
   targets: [
-  .target(name: "Either", dependencies: ["Prelude"]),
-  .testTarget(name: "EitherTests", dependencies: ["Either"]),
-  .target(name: "Optics", dependencies: ["Prelude"]),
-  .testTarget(name: "OpticsTests", dependencies: ["Optics"]),
-  .target(name: "Prelude", dependencies: []),
-  .testTarget(name: "PreludeTests", dependencies: ["Prelude"]),
-  ]
+    .target(name: "Either", dependencies: ["Prelude"]),
+    .testTarget(name: "EitherTests", dependencies: ["Either"]),
+    .target(name: "Optics", dependencies: ["Prelude"]),
+    .testTarget(name: "OpticsTests", dependencies: ["Optics"]),
+    .target(name: "Prelude", dependencies: []),
+    .testTarget(name: "PreludeTests", dependencies: ["Prelude"]),
+    ]
 )
+
