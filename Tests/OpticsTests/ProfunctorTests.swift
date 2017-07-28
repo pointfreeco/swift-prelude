11e846181aa78f1264222fe3ba792f55fe85206e
Brandon Williams <mbw234@gmail.com>
2017-07-28 10:51:59 -0400

    NOT FUN

--- Tests/OpticsTests/ProfunctorTests.swift
+++ Tests/OpticsTests/ProfunctorTests.swift
@@ -11,16 +11,13 @@ func some<A, B>(_ a2b: @escaping (A) -> B) -> (A?) -> Either<B?, B?> {
   return { some in some.map(a2b >>> Either.right) ?? .left(.none) }
 }
 
+// this is wrong v
 func ix<A>(_ idx: Int) -> Getter<[A], [A], A, A> {
   return { (forget: Forget<A, A, A>) -> Forget<A, [A], [A]> in
     Forget<A, [A], [A]> { (xs: [A]) -> A in
       forget.unwrap(xs[idx])
     }
   }
-}
-
-func left<A, B>() -> Getter<Either<A, B>, Either<A, B>, A, A> {
-  fatalError()
 }
 
 let incr = { $0 + 1 }
@@ -35,9 +32,10 @@ class ProfunctorTests: XCTestCase {
 
     dump(data |> traversed <<< left <<< traversed <<< some %~ incr)
 
-    dump(data .^ (traversed <<< left() <<< ix(1)))
-
     print("")
+
+    dump(data .^ ix(0))
+//    dump(data .^ (traversed <<< left <<< ix(0)))
 
     //dump(data .^ (traversed <<< left <<< traversed <<< some))
   }
