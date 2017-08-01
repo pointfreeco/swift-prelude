2d77d0507b24f85416ebd1df09f030cf0eed031f
Brandon Williams <mbw234@gmail.com>
2017-08-01 18:20:24 -0400

    clean up

--- Sources/Optics/Forget.swift
+++ Sources/Optics/Forget.swift
@@ -1,25 +1,7 @@
-import Prelude
-
 public struct Forget<R, A, B> {
   public let unwrap: (A) -> R
 
   public init(_ unwrap: @escaping (A) -> R) {
     self.unwrap = unwrap
   }
-
-  public func map<S>(_ f: @escaping (R) -> S) -> Forget<S, A, B> {
-    return .init(self.unwrap >>> f)
-  }
-
-  public func cmap<C>(_ f: @escaping (C) -> A) -> Forget<R, C, B> {
-    return .init(f >>> self.unwrap)
-  }
-}
-
-public func map<R, S, A, B>(_ f: @escaping (R) -> S) -> (Forget<R, A, B>) -> Forget<S, A, B> {
-  return { forget in forget.map(f) }
-}
-
-public func cmap<R, A, C, B>(_ f: @escaping (C) -> A) -> (Forget<R, A, B>) -> Forget<R, C, B> {
-  return { forget in forget.cmap(f) }
 }
