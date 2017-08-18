7f5696597069d64a9d0f3f41843f49bd35fc6581
Brandon Williams <mbw234@gmail.com>
2017-08-18 09:47:56 -0400

    Make Unit a monoid

--- Sources/Prelude/Unit.swift
+++ Sources/Prelude/Unit.swift
@@ -1,3 +1,11 @@
 public struct Unit {}
 
 public let unit = Unit()
+
+extension Unit: Monoid {
+  public static var empty: Prelude.Unit = unit
+  
+  public static func <> (lhs: Prelude.Unit, rhs: Prelude.Unit) -> Prelude.Unit {
+    return unit
+  }
+}
