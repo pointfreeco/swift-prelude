48385fc677064da300963fef28c7827386c49db2
Stephen Celis <stephen.celis@gmail.com>
2017-10-23 18:23:26 -0400

    Support more curry args (#69)

--- Sources/Prelude/Curry.swift
+++ Sources/Prelude/Curry.swift
@@ -1,19 +1,63 @@
-public func curry<A, B, C>(_ f: @escaping (A, B) -> C) -> (A) -> (B) -> C {
-  return { a in
-    { b in
-      f(a, b)
-    }
-  }
-}
-
-public func curry<A, B, C, D>(_ f: @escaping (A, B, C) -> D) -> (A) -> (B) -> (C) -> D {
-  return { a in
-    { b in
-      { c in
-        f(a, b, c)
+public func curry<A, B, C>(_ f: @escaping (A, B) -> C)
+  -> (A)
+  -> (B)
+  -> C {
+    return { a in
+      { b in
+        f(a, b)
       }
     }
-  }
+}
+
+public func curry<A, B, C, D>(_ f: @escaping (A, B, C) -> D)
+  -> (A)
+  -> (B)
+  -> (C)
+  -> D {
+    return { a in
+      { b in
+        { c in
+          f(a, b, c)
+        }
+      }
+    }
+}
+
+public func curry<A, B, C, D, E>(_ f: @escaping (A, B, C, D) -> E)
+  -> (A)
+  -> (B)
+  -> (C)
+  -> (D)
+  -> E {
+    return { a in
+      { b in
+        { c in
+          { d in
+            f(a, b, c, d)
+          }
+        }
+      }
+    }
+}
+
+public func curry<A, B, C, D, E, F>(_ f: @escaping (A, B, C, D, E) -> F)
+  -> (A)
+  -> (B)
+  -> (C)
+  -> (D)
+  -> (E)
+  -> (F) {
+    return { a in
+      { b in
+        { c in
+          { d in
+            { e in
+              f(a, b, c, d, e)
+            }
+          }
+        }
+      }
+    }
 }
 
 public func uncurry<A, B, C>(_ f: @escaping (A) -> (B) -> C) -> (A, B) -> C {
