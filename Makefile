e720e4818ac3708b44b8e2de762db5d89fa100f8
Brandon Williams <mbw234@gmail.com>
2017-11-16 09:08:57 -0500

    use sourcery to generate linuxmain

--- Makefile
+++ Makefile
@@ -1,2 +1,10 @@
 test:
-	docker build --tag swift-prelude . && docker run --rm swift-prelude
+	sourcery --sources ./Tests/ --templates ./.sourcery-templates/ --output ./Tests/ \
+		&& mv ./Tests/LinuxMain.generated.swift ./Tests/LinuxMain.swift \
+		&& swift test
+
+test-linux:
+	sourcery --sources ./Tests/ --templates ./.sourcery-templates/ --output ./Tests/ \
+		&& mv ./Tests/LinusMain.generated.swift ./Tests/LinuxMain.swift \
+		&& docker build --tag swift-prelude . \
+		&& docker run --rm swift-prelude
--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,30 +1,215 @@
-import XCTest
-@testable import PreludeTests
+// Generated using Sourcery 0.9.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
 
+import XCTest
+
+extension ArrayTests {
+  static var allTests: [(String, (ArrayTests) -> () throws -> Void)] = [
+  ]
+}
+extension ChoiceTests {
+  static var allTests: [(String, (ChoiceTests) -> () throws -> Void)] = [
+  ]
+}
+extension EitherTests {
+  static var allTests: [(String, (EitherTests) -> () throws -> Void)] = [
+    ("testEither", testEither),
+    ("testLeft", testLeft),
+    ("testRight", testRight),
+    ("testIsLeft", testIsLeft),
+    ("testIsRight", testIsRight),
+    ("testWrap", testWrap),
+    ("testUnwrap", testUnwrap),
+    ("testMap", testMap),
+    ("testApply", testApply),
+    ("testAlt", testAlt),
+    ("testPure", testPure),
+    ("testFlatMap", testFlatMap),
+    ("testAppend", testAppend)
+  ]
+}
+extension EquatableTests {
+  static var allTests: [(String, (EquatableTests) -> () throws -> Void)] = [
+    ("testEquatable", testEquatable),
+    ("testPerformance", testPerformance)
+  ]
+}
+extension EventTests {
+  static var allTests: [(String, (EventTests) -> () throws -> Void)] = [
+    ("testCombine", testCombine),
+    ("testMerge", testMerge),
+    ("testFilter", testFilter),
+    ("testReduce", testReduce),
+    ("testCount", testCount),
+    ("testWithLast", testWithLast),
+    ("testSampleOn", testSampleOn),
+    ("testMapOptional", testMapOptional),
+    ("testCatOptionals", testCatOptionals),
+    ("testMap", testMap),
+    ("testApply", testApply),
+    ("testAppend", testAppend),
+    ("testConcat", testConcat)
+  ]
+}
+extension FreeNearSemiringTests {
+  static var allTests: [(String, (FreeNearSemiringTests) -> () throws -> Void)] = [
+    ("testOp", testOp)
+  ]
+}
+extension FunctionTests {
+  static var allTests: [(String, (FunctionTests) -> () throws -> Void)] = [
+  ]
+}
+extension KeyPathTests {
+  static var allTests: [(String, (KeyPathTests) -> () throws -> Void)] = [
+    ("testGet", testGet),
+    ("testOver", testOver),
+    ("testSet", testSet)
+  ]
+}
+extension MonoidTests {
+  static var allTests: [(String, (MonoidTests) -> () throws -> Void)] = [
+  ]
+}
+extension NestedTests {
+  static var allTests: [(String, (NestedTests) -> () throws -> Void)] = [
+    ("testNested", testNested)
+  ]
+}
+extension NonEmptyTests {
+  static var allTests: [(String, (NonEmptyTests) -> () throws -> Void)] = [
+    ("testFirst", testFirst),
+    ("testLast", testLast),
+    ("testCount", testCount),
+    ("testSubscript", testSubscript),
+    ("testMutableNonEmptyArray", testMutableNonEmptyArray),
+    ("testMutableAppend", testMutableAppend),
+    ("testSet", testSet),
+    ("testString", testString),
+    ("testIterator", testIterator),
+    ("testUncons", testUncons),
+    ("testIso", testIso),
+    ("testMap", testMap),
+    ("testApply", testApply),
+    ("testPure", testPure),
+    ("testFlatMap", testFlatMap),
+    ("testInequality", testInequality),
+    ("testSemigroup", testSemigroup)
+  ]
+}
+extension OpticsTests {
+  static var allTests: [(String, (OpticsTests) -> () throws -> Void)] = [
+    ("testViewOn", testViewOn),
+    ("testIx", testIx),
+    ("testKey", testKey),
+    ("testElem", testElem),
+    ("testOver", testOver),
+    ("testSet", testSet),
+    ("testAddOver", testAddOver),
+    ("testSubOver", testSubOver),
+    ("testMulOver", testMulOver),
+    ("testDivOver", testDivOver),
+    ("testDisjOver", testDisjOver),
+    ("testConjOver", testConjOver),
+    ("testAppendOver", testAppendOver),
+    ("testTraversed", testTraversed),
+    ("testStrongPrisms", testStrongPrisms),
+    ("testChoicePrisms", testChoicePrisms),
+    ("testLots", testLots),
+    ("testGetters", testGetters)
+  ]
+}
+extension OptionalTests {
+  static var allTests: [(String, (OptionalTests) -> () throws -> Void)] = [
+  ]
+}
+extension ParallelTests {
+  static var allTests: [(String, (ParallelTests) -> () throws -> Void)] = [
+    ("testParallel", testParallel),
+    ("testRace", testRace)
+  ]
+}
+extension ReaderTests {
+  static var allTests: [(String, (ReaderTests) -> () throws -> Void)] = [
+  ]
+}
+extension SemigroupTests {
+  static var allTests: [(String, (SemigroupTests) -> () throws -> Void)] = [
+  ]
+}
+extension SequenceTests {
+  static var allTests: [(String, (SequenceTests) -> () throws -> Void)] = [
+    ("testCatOptionals", testCatOptionals),
+    ("testMapOptional", testMapOptional),
+    ("testConcat", testConcat),
+    ("testIntersperse", testIntersperse)
+  ]
+}
+extension StateTests {
+  static var allTests: [(String, (StateTests) -> () throws -> Void)] = [
+  ]
+}
+extension StringTests {
+  static var allTests: [(String, (StringTests) -> () throws -> Void)] = [
+    ("testJoined", testJoined)
+  ]
+}
+extension StrongTests {
+  static var allTests: [(String, (StrongTests) -> () throws -> Void)] = [
+  ]
+}
+extension TupleTests {
+  static var allTests: [(String, (TupleTests) -> () throws -> Void)] = [
+    ("testTuples", testTuples)
+  ]
+}
+extension UnitTests {
+  static var allTests: [(String, (UnitTests) -> () throws -> Void)] = [
+  ]
+}
+extension ValidationNearSemiringTests {
+  static var allTests: [(String, (ValidationNearSemiringTests) -> () throws -> Void)] = [
+    ("testValidData", testValidData),
+    ("testInvalidData", testInvalidData)
+  ]
+}
+extension ValidationSemigroupTests {
+  static var allTests: [(String, (ValidationSemigroupTests) -> () throws -> Void)] = [
+    ("testValidData", testValidData),
+    ("testInvalidData", testInvalidData)
+  ]
+}
+extension WriterTests {
+  static var allTests: [(String, (WriterTests) -> () throws -> Void)] = [
+  ]
+}
+
+// swiftlint:disable trailing_comma
 XCTMain([
-  testCase(EquatableTests.allTests),
   testCase(ArrayTests.allTests),
+  testCase(ChoiceTests.allTests),
+  testCase(EitherTests.allTests),
+  testCase(EquatableTests.allTests),
+  testCase(EventTests.allTests),
   testCase(FreeNearSemiringTests.allTests),
   testCase(FunctionTests.allTests),
   testCase(KeyPathTests.allTests),
   testCase(MonoidTests.allTests),
+  testCase(NestedTests.allTests),
+  testCase(NonEmptyTests.allTests),
+  testCase(OpticsTests.allTests),
   testCase(OptionalTests.allTests),
   testCase(ParallelTests.allTests),
+  testCase(ReaderTests.allTests),
   testCase(SemigroupTests.allTests),
   testCase(SequenceTests.allTests),
+  testCase(StateTests.allTests),
   testCase(StringTests.allTests),
   testCase(StrongTests.allTests),
   testCase(TupleTests.allTests),
   testCase(UnitTests.allTests),
-  testCase(ChoiceTests.allTests),
-  testCase(EitherTests.allTests),
-  testCase(NestedTests.allTests),
-  testCase(StateTests.allTests),
-  testCase(OpticsTests.allTests),
-  testCase(ValidationSemigroupTests.allTests),
-  testCase(ReaderTests.allTests),
   testCase(ValidationNearSemiringTests.allTests),
-  testCase(NonEmptyTests.allTests),
-  testCase(TupleTests.allTests),
-  testCase(EventTests.allTests),
+  testCase(ValidationSemigroupTests.allTests),
+  testCase(WriterTests.allTests),
 ])
+// swiftlint:enable trailing_comma
