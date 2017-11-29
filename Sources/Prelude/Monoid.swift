0538bd01b3444cb3f7d5da72fef1071a8a02a7b2
Stephen Celis <stephen@stephencelis.com>
2017-11-28 23:14:04 -0500

    Add Semigroup/Monoid instance of Dictionary

--- Sources/Prelude/Monoid.swift
+++ Sources/Prelude/Monoid.swift
@@ -2,12 +2,16 @@ public protocol Monoid: Semigroup {
   static var empty: Self { get }
 }
 
-extension String: Monoid {
-  public static let empty = ""
-}
-
 extension Array: Monoid {
   public static var empty: Array { return [] }
+}
+
+extension Dictionary: Monoid {
+  public static var empty: Dictionary { return [:] }
+}
+
+extension String: Monoid {
+  public static let empty = ""
 }
 
 public func joined<M: Monoid>(_ s: M) -> ([M]) -> M {
