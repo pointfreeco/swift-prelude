dd57083fc276b117f783d3b151575fe66ffc3c59
Stephen Celis <stephen@stephencelis.com>
2018-04-19 21:10:25 -0400

    Overloads

--- Sources/Optics/Setter.swift
+++ Sources/Optics/Setter.swift
@@ -28,8 +28,30 @@ public func lens<S, T, A, B>(_ get: @escaping (S) -> A, _ set: @escaping (S, B) 
 ///   - over: A transformation function from the focus of a setter to a new value.
 /// - Returns: A function that takes a source and returns a target by mapping a function over the focus of a
 ///   setter.
+public func over<S, T, A, B>(_ setter: Setter<S, T, A, B>, _ f: @escaping (A) -> B) -> (S) -> T {
+  return setter(f)
+}
+
+/// Maps a function over the focus of a setter.
+///
+/// - Parameters:
+///   - setter: A setter.
+///   - over: A transformation function from the focus of a setter to a new value.
+/// - Returns: A function that takes a source and returns a target by mapping a function over the focus of a
+///   setter.
 public func %~ <S, T, A, B>(setter: Setter<S, T, A, B>, over: @escaping (A) -> B) -> (S) -> T {
   return setter(over)
+}
+
+/// Sets the focus of a setter to a constant value.
+///
+/// - Parameters:
+///   - setter: A setter.
+///   - set: A new value to replace the focus of a setter.
+/// - Returns: A function that takes a source and returns a target by replacing the focus of a setter with the
+///   given value.
+public func set<S, T, A, B>(_ setter: Setter<S, T, A, B>, _ value: B) -> (S) -> T {
+  return over(setter, const(value))
 }
 
 /// Sets the focus of a setter to a constant value.
