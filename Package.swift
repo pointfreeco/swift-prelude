8d597b051935dc7d63ec7cb6edefbeac07a73a94
Stephen Celis <stephen.celis@gmail.com>
2018-05-29 13:01:58 -0400

    Custom snapshot format (#106)
    * Update snapshot format
    
    * Bump snapshot testing
    
    * Bump snapshot testing

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     .library(name: "Writer", targets: ["Writer"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", .revision("0a86107")),
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", .revision("2a0edb4")),
     .package(url: "https://github.com/pointfreeco/swift-tagged.git", from: "0.1.0"),
   ],
   targets: [
