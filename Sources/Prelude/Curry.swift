5ed884b94f642a7251c96871c11629c35777f07e
Brandon Williams <mbw234@gmail.com>
2018-06-05 07:01:38 -0400

    more revert

--- Sources/Prelude/Curry.swift
+++ Sources/Prelude/Curry.swift
@@ -9,10 +9,6 @@ public func curry<A, B, C>(_ function: @escaping (A, B) -> C)
     }
 }
 
-func test() {
-  _ = curry(String.init(data:encoding:))
-}
-
 public func curry<A, B, C, D>(_ function: @escaping (A, B, C) -> D)
   -> (A)
   -> (B)
