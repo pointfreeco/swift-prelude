9e0df98f1d75628361d601f991e1a64dca08ac32
Stephen Celis <stephen.celis@gmail.com>
2018-11-08 16:00:01 -0500

    Lock to Swift 4.1 (#122)
    * Lock to Swift 4.0
    
    * Regress
    
    * Lock to Swift 4.1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:4.1
 import PackageDescription
 
 let package = Package(
@@ -16,7 +16,7 @@ let package = Package(
     .library(name: "Writer", targets: ["Writer"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", .revision("afb1918")),
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", .revision("d886237")),
     .package(url: "https://github.com/pointfreeco/swift-nonempty.git", from: "0.1.2"),
     .package(url: "https://github.com/pointfreeco/swift-tagged.git", from: "0.2.0"),
   ],
