db8ec5adacb19c37745d7069ea5fde2e2ed668ff
Brandon Williams <mbw234@gmail.com>
2018-12-19 10:16:32 -0500

    Update snapshot testing to 1.1.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     .library(name: "Writer", targets: ["Writer"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", .revision("ca1a008")),
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.1.0"),
     .package(url: "https://github.com/pointfreeco/swift-nonempty.git", from: "0.1.2"),
     .package(url: "https://github.com/pointfreeco/swift-tagged.git", from: "0.2.0"),
   ],
