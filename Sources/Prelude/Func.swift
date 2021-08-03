360e72ad60e7842a7f52f1bb926c22ecd049fc50
maximkrouk <id.maximkrouk@gmail.com>
2021-08-03 21:54:40 +0300

    feat(Func): Add protocol-based implementation
    - FuncProtocol renamed to Function
    - Func operators rewriten to work for any Function type
    - Added type-erasure to Function protocol

--- Sources/Prelude/Func.swift
+++ Sources/Prelude/Func.swift
@@ -1,24 +1,4 @@
-public protocol FuncProtocol {
-  associatedtype A
-  associatedtype B
-  typealias Signature = (A) -> B
-  var call: Signature { get }
-  init(_ call: @escaping Signature)
-}
-
-extension FuncProtocol {
-  public func callAsFunction(_ input: A) -> B {
-    return call(input)
-  }
-}
-
-extension FuncProtocol where A == Void {
-  public func callAsFunction() -> B {
-    return call(())
-  }
-}
-
-public struct Func<A, B>: FuncProtocol {
+public struct Func<A, B>: Function {
   public let call: (A) -> B
 
   public init(_ call: @escaping (A) -> B) {
@@ -26,45 +6,77 @@ public struct Func<A, B>: FuncProtocol {
   }
 }
 
-extension Func /* : Semigroupoid */ {
-  public static func >>> <C>(f: Func, g: Func<B, C>) -> Func<A, C> {
+public protocol Function {
+  associatedtype A
+  associatedtype B
+  typealias Signature = (A) -> B
+  var call: Signature { get }
+  init(_ call: @escaping Signature)
+}
+
+extension Function {
+  public func eraseToFunc() -> Func<A, B> {
+    return Func(call)
+  }
+}
+
+extension Function {
+  public func callAsFunction(_ input: A) -> B {
+    return call(input)
+  }
+}
+
+extension Function where A == Void {
+  public func callAsFunction() -> B {
+    return call(())
+  }
+}
+
+extension Function /* : Semigroupoid */ {
+  public static func >>> <G: Function>(f: Self, g: G) -> Func<A, G.B> where G.A == B {
     return .init(f.call >>> g.call)
   }
 
-  public static func <<< <C>(f: Func<B, C>, g: Func) -> Func<A, C> {
+  public static func <<< <F: Function>(f: F, g: Self) -> Func<A, F.B> where F.A == B {
     return .init(f.call <<< g.call)
   }
 }
 
-extension Func /* : Functor */ {
+extension Function /* : Functor */ {
   public func map<C>(_ f: @escaping (B) -> C) -> Func<A, C> {
     return .init(self.call >>> f)
   }
 
-  public static func <¢> <C>(f: @escaping (B) -> C, g: Func) -> Func<A, C> {
+  public static func <¢> <C>(f: @escaping (B) -> C, g: Self) -> Func<A, C> {
     return g.map(f)
   }
 }
 
-public func map<A, B, C>(_ f: @escaping (B) -> C) -> (Func<A, B>) -> Func<A, C> {
+public func map<A, B, C, F: Function>(_ f: @escaping (B) -> C) -> (F) -> Func<A, C>
+where F.A == A, F.B == B {
   return { $0.map(f) }
 }
 
-extension Func /* : Contravariant */ {
+extension Function {
   public func contramap<Z>(_ f: @escaping (Z) -> A) -> Func<Z, B> {
     return .init(f >>> self.call)
   }
+}
 
+// Have to implement in on a Func type directly due to compilation error:
+// - Member operator '>¢<' of protocol 'Function' must have at least one argument of type 'Self'
+extension Func /* : Contravariant */ {
   public static func >¢< <A, B, C>(f: @escaping (B) -> A, g: Func<A, C>) -> Func<B, C> {
     return g.contramap(f)
   }
 }
 
-public func contramap<A, B, C>(_ f: @escaping (B) -> A) -> (Func<A, C>) -> Func<B, C> {
+public func contramap<A, B, C, G: Function>(_ f: @escaping (B) -> A) -> (G) -> Func<B, C>
+where G.A == A, G.B == C {
   return { $0.contramap(f) }
 }
 
-extension Func /* : Profunctor */ {
+extension Function /* : Profunctor */ {
   public func dimap<Z, C>(_ f: @escaping (Z) -> A, _ g: @escaping (B) -> C) -> Func<Z, C> {
     return .init(f >>> self.call >>> g)
   }
@@ -76,67 +88,84 @@ public func dimap<A, B, C, D>(
   )
   -> (Func<B, C>)
   -> Func<A, D> {
-
     return { $0.dimap(f, g) }
 }
 
-extension Func /* : Apply */ {
-  public func apply<C>(_ f: Func<A, Func<B, C>>) -> Func<A, C> {
+extension Function /* : Apply */ {
+  public func apply<F: Function, G: Function, C>(_ f: F) -> Func<A, C>
+  where F.A == A, F.B == G, G.A == B, G.B == C {
     return .init { a in f.call(a).call(self.call(a)) }
   }
 
-  public static func <*> <C>(f: Func<A, Func<B, C>>, x: Func) -> Func<A, C> {
+  public static func <*> <F: Function, G: Function, C>(f: F, x: Self) -> Func<A, C>
+  where F.A == A, F.B == G, G.A == B, G.B == C {
     return x.apply(f)
   }
 }
 
-public func apply<A, B, C>(_ f: Func<A, Func<B, C>>) -> (Func<A, B>) -> Func<A, C> {
+public func apply<F: Function, G: Function, B, C>(_ f: F) -> (Func<F.A, B>) -> Func<F.A, C>
+where F.B == G, G.A == B, G.B == C {
   return { $0.apply(f) }
 }
 
 // MARK: Applicative
+extension Function {
+  public static func pure(_ b: B) -> Self {
+    .init(const(b))
+  }
+}
+
 public func pure<A, B>(_ b: B) -> Func<A, B> {
   return .init(const(b))
 }
 
-extension Func /* : Monad */ {
-  public func flatMap<C>(_ f: @escaping (B) -> Func<A, C>) -> Func<A, C> {
+extension Function /* : Monad */ {
+  public func flatMap<F: Function>(_ f: @escaping (B) -> F) -> Func<A, F.B>
+  where F.A == A {
     return .init { f(self.call($0)).call($0) }
   }
 }
 
-public func flatMap<A, B, C>(_ f: @escaping (B) -> Func<A, C>) -> (Func<A, B>) -> Func<A, C> {
+public func flatMap<F: Function, C>(_ f: @escaping (C) -> F) -> (Func<F.A, C>) -> Func<F.A, F.B> {
   return { $0.flatMap(f) }
 }
 
-extension Func: Semigroup where B: Semigroup {
-  public static func <> (f: Func, g: Func) -> Func {
+extension Function where B: Semigroup {
+  public static func <> (f: Self, g: Self) -> Self {
     return .init { f.call($0) <> g.call($0) }
   }
 }
 
-extension Func: Monoid where B: Monoid {
-  public static var empty: Func {
+extension Func: Semigroup where B: Semigroup {}
+
+extension Function where B: Monoid {
+  public static var empty: Self {
     return .init(const(B.empty))
   }
 }
+  
+extension Func: Monoid where B: Monoid {}
 
-extension Func: NearSemiring where B: NearSemiring {
-  public static func + (f: Func, g: Func) -> Func {
+extension Function where B: NearSemiring {
+  public static func + (f: Self, g: Self) -> Self {
     return .init { f.call($0) + g.call($0) }
   }
 
-  public static func * (f: Func, g: Func) -> Func {
+  public static func * (f: Self, g: Self) -> Self {
     return .init { f.call($0) * g.call($0) }
   }
 
-  public static var zero: Func {
+  public static var zero: Self {
     return .init(const(B.zero))
   }
 }
 
-extension Func: Semiring where B: Semiring {
-  public static var one: Func {
+extension Func: NearSemiring where B: NearSemiring {}
+
+extension Function where B: Semiring {
+  public static var one: Self {
     return .init(const(B.one))
   }
 }
+
+extension Func: Semiring where B: Semiring {}
--- Tests/PreludeTests/FunctionTests.swift
+++ Tests/PreludeTests/FunctionTests.swift
@@ -2,11 +2,46 @@ import XCTest
 import Prelude
 
 class FunctionTests: XCTestCase {
+  struct IntToString: Function {
+    static let live = IntToString(String.init)
+    
+    typealias Signature = (Int) -> String
+    
+    init(_ call: @escaping Signature) {
+      self.call = call
+    }
+    
+    let call: Signature
+    
+    func callAsFunction(integerValue: Int) -> String {
+      call(integerValue)
+    }
+  }
+  
   public func testCallAsFunction() {
     let increment = Func<Int, Int> { $0 + 1 }
     let zero = Func<Void, Int> { 0 }
     
     XCTAssertEqual(increment(0), 1)
     XCTAssertEqual(zero(), 0)
+  }
+  
+  public func testCustomCallAsFunction() {
+    let convert = IntToString.live
+    
+    XCTAssertEqual(convert(0), "0")
+    XCTAssertEqual(convert(integerValue: 1), "1")
+  }
+  
+  public func testTypeErasure() {
+    let convert = IntToString.live
+    let erasedConvert = convert.eraseToFunc()
+    
+    func typeIsFuncOfIntToString<F: Function>(_ f: F) -> Bool {
+      return f is Func<Int, String>
+    }
+    
+    XCTAssertTrue(typeIsFuncOfIntToString(erasedConvert))
+    XCTAssertEqual(convert(0), erasedConvert(0))
   }
 }
