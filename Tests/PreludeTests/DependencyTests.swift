f3a3706fe50415aabf99466b721ac79c6021b264
Brandon Williams <mbrandonw@hey.com>
2022-12-31 13:13:18 -0800

    wip

--- Tests/PreludeTests/DependencyTests.swift
+++ Tests/PreludeTests/DependencyTests.swift
@@ -3,18 +3,19 @@ import Prelude
 import XCTest
 
 final class DependencyTests: XCTestCase {
-  func testIOPropagation() {
-    let operation = DependencyValues.withTestValues {
+  func testIOPropagation() async {
+    let operation = withDependencyValues {
       $0.date.now = Date(timeIntervalSince1970: 1234567890)
     } operation: {
       IO<TimeInterval> { DependencyValues._current.date.now.timeIntervalSince1970 }
     }
 
-    XCTAssertEqual(operation.perform(), 1234567890)
+    let value = await operation.performAsync()
+    XCTAssertEqual(value, 1234567890)
   }
 
   func testParallelPropagation() {
-    let parallel = DependencyValues.withTestValues {
+    let parallel = withDependencyValues {
       $0.date.now = Date(timeIntervalSince1970: 1234567890)
     } operation: {
       Parallel<TimeInterval> { $0(DependencyValues._current.date.now.timeIntervalSince1970) }
